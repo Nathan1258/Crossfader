4f7a7648c94caf7e73565d31cc5a4e01fdff2155
Nathan <N1043109@my.ntu.ac.uk>
2023-05-18 12:03:35 +0100

    Initial Commit

---
+++ Crossfader/CrossfaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossfaderApp.swift
+//  Crossfader
+//
+//  Created by Nathan Ellis on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CrossfaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
